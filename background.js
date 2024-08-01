906d6d722aeab993f3443bf0e71c43cabf81208f
Taichi Maeda <taichi.maeda.up@gmail.com>
2024-08-01 14:23:00 +0900

    Add remind feature

--- background.js
+++ background.js
@@ -14,6 +14,11 @@ chrome.runtime.onInstalled.addListener(() => {
     title: "Clear all PRs",
     contexts: ["all"],
   });
+  chrome.contextMenus.create({
+    id: "remind",
+    title: "Remind me of this PR",
+    contexts: ["all"],
+  });
 });
 
 chrome.contextMenus.onClicked.addListener((info, tab) => {
@@ -21,22 +26,31 @@ chrome.contextMenus.onClicked.addListener((info, tab) => {
     case "highlight":
       chrome.scripting.executeScript({
         target: { tabId: tab.id },
-        function: addHash,
+        function: highlight,
       });
       break;
     case "unhighlight":
       chrome.scripting.executeScript({
         target: { tabId: tab.id },
-        function: removeHash,
+        function: unhighlight,
       });
       break;
     case "clear":
-      chrome.storage.local.set({ text: "" });
+      chrome.scripting.executeScript({
+        target: { tabId: tab.id },
+        function: clear,
+      });
+      break;
+    case "remind":
+      chrome.scripting.executeScript({
+        target: { tabId: tab.id },
+        function: remind,
+      });
       break;
   }
 });
 
-function addHash() {
+function highlight() {
   // NOTE: This cannot be extracted to a function because of `chrome.scripting.executeScript`
   let hash;
   const divEls = document.querySelectorAll("div");
@@ -47,7 +61,7 @@ function addHash() {
     }
   }
   if (hash === undefined) {
-    window.alert("Unable to find the merge commit hash");
+    alert("Unable to find the merge commit hash");
   }
 
   chrome.storage.local.get("text", (data) => {
@@ -61,7 +75,33 @@ function addHash() {
   });
 }
 
-function removeHash() {
+function unhighlight() {
+  // NOTE: This cannot be extracted to a function because of `chrome.scripting.executeScript`
+  let hash;
+  const divEls = document.querySelectorAll("div");
+  for (const divEl of divEls) {
+    if (divEl.textContent.includes("merged commit")) {
+      const aEl = divEl.querySelector("a > code");
+      hash = aEl.textContent.trim();
+    }
+  }
+  if (hash === undefined) {
+    alert("Unable to find the merge commit hash");
+  }
+
+  chrome.storage.local.get("text", (data) => {
+    const text = data.text || "";
+    const lines = text.split("\n").filter((line) => line !== hash);
+    const newText = lines.join("\n");
+    chrome.storage.local.set({ text: newText });
+  });
+}
+
+function clear() {
+  chrome.storage.local.set({ text: "" });
+}
+
+function remind() {
   // NOTE: This cannot be extracted to a function because of `chrome.scripting.executeScript`
   let hash;
   const divEls = document.querySelectorAll("div");
@@ -75,10 +115,7 @@ function removeHash() {
     window.alert("Unable to find the merge commit hash");
   }
 
-  chrome.storage.local.get("text", (data) => {
-    const text = data.text || "";
-    const lines = text.split("\n").filter((line) => line !== hash);
-    const newText = lines.join("\n");
-    chrome.storage.local.set({ text: newText });
-  });
+  setTimeout(() => {
+    alert(`Don't forget to check PR: ${hash}`);
+  }, 1000 * 60 * 30);
 }
